b1178c33c14ce0edf5b091cd655497451f4459ea
cesar.peralta <cesar.peralta@cakedc.com>
2023-08-05 11:19:34 -0400

    enabling sequelize logging

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize')
 const db = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
     host: process.env.MYSQLHOST,
     dialect: 'mysql',
-    logging: false
+    // logging: false
 })
 
 module.exports = { db }
