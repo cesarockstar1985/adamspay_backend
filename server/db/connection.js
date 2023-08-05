5177db9704b2af903f91e01621bfc924dba32cd6
cesar.peralta <cesar.peralta@cakedc.com>
2023-08-05 11:34:40 -0400

    fixing wrong port number

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,11 +3,9 @@ const { Sequelize } = require('sequelize')
 const db = new Sequelize(
     process.env.MYSQLDATABASE,
     process.env.MYSQLUSER,
-    process.env.MYSQLPASSWORD,
-    process.env.MYSQLPORT,{
+    process.env.MYSQLPASSWORD,{
         host: process.env.MYSQLHOST,
-        dialect: 'mysql',
-        // logging: false
+        port: process.env.MYSQLPORT,
 })
 
 module.exports = { db }
