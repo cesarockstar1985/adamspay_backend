61fdcc3e0d3db32d128c54c284536dbd71071a06
cesar.peralta <cesar.peralta@cakedc.com>
2023-08-05 11:27:27 -0400

    add specify Sequelize connection port

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,9 +1,13 @@
 const { Sequelize } = require('sequelize')
 
-const db = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
-    host: process.env.MYSQLHOST,
-    dialect: 'mysql',
-    // logging: false
+const db = new Sequelize(
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
+    process.env.MYSQLPORT,{
+        host: process.env.MYSQLHOST,
+        dialect: 'mysql',
+        // logging: false
 })
 
 module.exports = { db }
